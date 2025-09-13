ea21f050146733fc98097496677df74a31616806
Aigio Liu <lysoft.pw@foxmail.com>
2025-09-13 16:30:10 +0800

    🚧 Misc

---
+++ src/AigioL.Common.AspNetCore.AppCenter.Identity.Core/Models/Request/RegisterByEmailRequestV0.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AigioL.Common.AspNetCore.AppCenter.Identity.Models.Request;
+class RegisterByEmailRequestV0
+{
+}
---
+++ src/AigioL.Common.AspNetCore.AppCenter.Identity.Core/Models/Request/ValidateRegisterEmailRequestV0.cs
@@ -0,0 +1,11 @@
+namespace AigioL.Common.AspNetCore.AppCenter.Identity.Models.Request;
+
+[global::MemoryPack.MemoryPackable(global::MemoryPack.GenerateType.Object, global::MemoryPack.SerializeLayout.Explicit)]
+public sealed partial class ValidateRegisterEmailRequestV0
+{
+    /// <summary>
+    /// 邮箱地址
+    /// </summary>
+    [global::MemoryPack.MemoryPackOrder(0)]
+    public required string Email { get; set; }
+}
