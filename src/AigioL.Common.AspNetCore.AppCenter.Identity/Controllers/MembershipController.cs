8e83aae39e02828aac8c41887dc695dde1978a83
LY <liyu971652764@outlook.com>
2025-12-15 17:43:36 +0800

    ✨ 会员验证接口

--- src/AigioL.Common.AspNetCore.AppCenter.Identity/Controllers/MembershipController.cs
+++ src/AigioL.Common.AspNetCore.AppCenter.Identity/Controllers/MembershipController.cs
@@ -1,9 +1,10 @@
-using AigioL.Common.AspNetCore.AppCenter.Constants;
 using AigioL.Common.AspNetCore.AppCenter.Identity.Models.Membership;
 using AigioL.Common.AspNetCore.AppCenter.Identity.Repositories.Abstractions;
 using AigioL.Common.Models;
-using MemoryPack;
+using AigioL.Common.Primitives.Columns;
+using Microsoft.AspNetCore.Mvc;
 using StackExchange.Redis;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
 using R = AigioL.Common.AspNetCore.AppCenter.Identity.UI.Properties.Resources;
 
@@ -21,12 +22,50 @@ public static partial class MembershipController
         var routeGroup = b.MapGroup(pattern)
             .RequireAuthorization(MSMinimalApis.ApiControllerBaseAuthorize);
 
+        routeGroup.MapPost("v/{tbd}", async (HttpContext context,
+            [FromRoute][StringLength(MaxLengths.SHA384)] string tbd) =>
+        {
+            // tbd 待定传参，保留值
+            var r = await GetUserIsMembershipAsync(context, context.RequestAborted);
+            return r;
+        }).WithDescription("获取用户是否是会员")
+        .WithRequiredSecurityKey();
         routeGroup.MapGet("info", async (HttpContext context) =>
         {
             var r = await GetUserMembershipInfoAsync(context, context.RequestAborted);
             return r;
         }).WithDescription("获取会员信息")
         .WithRequiredSecurityKey();
+    }
+
+    /// <summary>
+    /// 获取用户是否是会员
+    /// </summary>
+    static async Task<ApiRsp<bool>> GetUserIsMembershipAsync(
+        HttpContext context,
+        CancellationToken cancellationToken = default)
+    {
+        var userId = context.GetUserId();
+        if (userId == null)
+            return ApiRspCode.Unauthorized;
+
+        var logger = context.RequestServices.GetRequiredService<ILoggerFactory>().CreateLogger(nameof(MembershipController));
+        var userMembershipRepo = context.RequestServices.GetRequiredService<IUserMembershipRepository>();
+        var connection = context.RequestServices.GetRequiredService<IConnectionMultiplexer>();
+
+        (MembershipInfo? membershipInfo, var _) = await userMembershipRepo.GetUserMembershipCachePriorityAsync(
+            logger,
+            connection,
+            userId.Value,
+            false,
+            cancellationToken);
+
+        var isMembership = membershipInfo != null && membershipInfo.IsMembership;
+        return new ApiRsp<bool>
+        {
+            Code = unchecked((uint)ApiRspCode.OK),
+            Content = isMembership,
+        };
     }
 
     /// <summary>
