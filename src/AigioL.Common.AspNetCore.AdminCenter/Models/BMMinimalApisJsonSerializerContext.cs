c4e1d6ce275c1e0e272260558315227a65cb3130
LY <liyu971652764@outlook.com>
2026-08-08 18:28:58 +0800

    🧹 清理 GameTrainer 相关序列化特性及引用
    移除 GameTrainer.Models.AdminCenter 的 using 引用，删除与 GameTrainer 相关的 JsonSerializable 特性，保留其他控制器相关特性，并为 BMMinimalApisJsonSerializerContext 添加注释说明。

--- src/AigioL.Common.AspNetCore.AdminCenter/Models/BMMinimalApisJsonSerializerContext.cs
+++ src/AigioL.Common.AspNetCore.AdminCenter/Models/BMMinimalApisJsonSerializerContext.cs
@@ -14,7 +14,6 @@ using AigioL.Common.AspNetCore.AppCenter.Ordering.Models;
 using AigioL.Common.AspNetCore.AppCenter.Ordering.Models.Membership;
 using AigioL.Common.JsonWebTokens.Models;
 using AigioL.Common.Primitives.Models;
-using GameTrainer.Models.AdminCenter;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -170,12 +169,6 @@ namespace AigioL.Common.AspNetCore.AdminCenter.Models;
 [JsonSerializable(typeof(BMApiRsp<KomaasharuEdit?>))]
 [JsonSerializable(typeof(BMApiRsp<StatisticsKomaasharuResponse[]?>))]
 #endregion
-#region GameTrainerController
-[JsonSerializable(typeof(BMApiRsp<PagedModel<GameTrainerTableItem>?>))]
-//[JsonSerializable(typeof(GameTrainerEditModel))]
-[JsonSerializable(typeof(BMApiRsp<GameTrainerDetailModel?>))]
-[JsonSerializable(typeof(BMApiRsp<IEnumerable<GameTrainerOptionItem>?>))]
-#endregion
 #region MembershipBusinessOrderController
 [JsonSerializable(typeof(BMApiRsp<PagedModel<MembershipBusinessOrderTableItem>?>))]
 #endregion
@@ -211,7 +204,7 @@ namespace AigioL.Common.AspNetCore.AdminCenter.Models;
 [JsonSerializable(typeof(AddRefundBillModel))]
 #endregion
 [JsonSourceGenerationOptions]
-public sealed partial class BMMinimalApisJsonSerializerContext : JsonSerializerContext
+public sealed partial class BMMinimalApisJsonSerializerContext : JsonSerializerContext // 管理后台通用共享的模型类的源生成上下文
 {
     static BMMinimalApisJsonSerializerContext()
     {
