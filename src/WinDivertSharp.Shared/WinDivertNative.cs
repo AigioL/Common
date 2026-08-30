80be9cba9d233791dd811cb63b2a5ca02cc62fa8
LY <liyu971652764@outlook.com>
2026-08-30 22:20:14 +0800

    📝 优化 WinDivertNative.cs 的方法注释为结构化 XML 文档

--- src/WinDivertSharp.Shared/WinDivertNative.cs
+++ src/WinDivertSharp.Shared/WinDivertNative.cs
@@ -46,161 +46,192 @@ internal static unsafe class WinDivertNative
 {
     const string WinDivert = "WinDivert.dll";
 
-    ///// <summary>
-    ///// Static initializer to get our paths correct.
-    ///// </summary>
-    //static WinDivertNative()
-    //{
-    //    var baseDir = AppContext.BaseDirectory;
-    //    var libraryPath = Path.Combine(baseDir, WinDivert);
-    //    if (!File.Exists(libraryPath))
-    //        libraryPath = PathCompat.Combine(baseDir, RuntimeInformation.ProcessArchitecture.ToString(), WinDivert);
-    //    if (!File.Exists(libraryPath))
-    //        throw new FileNotFoundException(null, libraryPath);
-    //    NativeLibrary.Load(libraryPath);
-    //}
-
-    /// Return Type: HANDLE->void*
-    ///filter: char*
-    ///layer: WINDIVERT_LAYER->Anonymous_13846946_b76a_4250_9642_c2122691f126
-    ///priority: INT16->short
-    ///flags: UINT64->unsigned __int64
+    /// <summary>
+    /// Open a WinDivert handle.
+    /// </summary>
+    /// <param name="filter">Filter string.</param>
+    /// <param name="layer">Packet processing layer.</param>
+    /// <param name="priority">Handle priority.</param>
+    /// <param name="flags">Open flags.</param>
+    /// <returns>Handle to the WinDivert instance, or <see cref="nint.Zero"/> on failure.</returns>
     [DllImport(WinDivert, EntryPoint = "WinDivertOpen", CallingConvention = CallingConvention.Cdecl, SetLastError = true)]
-    public static extern IntPtr WinDivertOpen([In()][MarshalAs(UnmanagedType.LPStr)] string filter, WinDivertLayer layer, short priority, ulong flags);
+    public static extern nint WinDivertOpen([In][MarshalAs(UnmanagedType.LPStr)] string filter, WinDivertLayer layer, short priority, ulong flags);
 
-    /// Return Type: BOOL->int
-    ///handle: HANDLE->void*
-    ///pPacket: PVOID->void*
-    ///packetLen: UINT->unsigned int
-    ///pAddr: PWINDIVERT_ADDRESS->Anonymous_33ad92c9_0104_417e_989a_2fdd4b3efcc1*
-    ///readLen: ref uint
+    /// <summary>
+    /// Receive (read) a packet from a WinDivert handle.
+    /// </summary>
+    /// <param name="handle">WinDivert handle.</param>
+    /// <param name="pPacket">Buffer that receives the packet.</param>
+    /// <param name="packetLen">Size of <paramref name="pPacket"/> in bytes.</param>
+    /// <param name="pAddr">Receives packet metadata.</param>
+    /// <param name="readLen">Receives number of bytes read.</param>
+    /// <returns><see langword="true"/> on success; otherwise <see langword="false"/>.</returns>
     [DllImport(WinDivert, EntryPoint = "WinDivertRecv", CallingConvention = CallingConvention.Cdecl, SetLastError = true)]
     [return: MarshalAs(UnmanagedType.Bool)]
-    public static extern bool WinDivertRecv([In()] IntPtr handle, IntPtr pPacket, uint packetLen, [In()] ref WinDivertAddress pAddr, ref uint readLen);
+    public static extern bool WinDivertRecv([In] nint handle, nint pPacket, uint packetLen, [In] ref WinDivertAddress pAddr, ref uint readLen);
 
-    /// Return Type: BOOL->int
-    ///handle: HANDLE->void*
-    ///pPacket: PVOID->void*
-    ///packetLen: UINT->unsigned int
-    ///flags: UINT64->unsigned __int64
-    ///pAddr: PWINDIVERT_ADDRESS->Anonymous_33ad92c9_0104_417e_989a_2fdd4b3efcc1*
-    ///readLen: ref uint
-    ///lpOverlapped: LPOVERLAPPED->_OVERLAPPED*
+    /// <summary>
+    /// Receive (read) a packet from a WinDivert handle.
+    /// </summary>
+    /// <param name="handle">WinDivert handle.</param>
+    /// <param name="pPacket">Buffer that receives the packet.</param>
+    /// <param name="packetLen">Size of <paramref name="pPacket"/> in bytes.</param>
+    /// <param name="flags">Receive flags.</param>
+    /// <param name="pAddr">Receives packet metadata.</param>
+    /// <param name="readLen">Receives number of bytes read.</param>
+    /// <param name="lpOverlapped">Overlapped I/O structure for asynchronous operations.</param>
+    /// <returns><see langword="true"/> on success; otherwise <see langword="false"/>.</returns>
     [DllImport(WinDivert, EntryPoint = "WinDivertRecvEx", CallingConvention = CallingConvention.Cdecl, SetLastError = true)]
     [return: MarshalAs(UnmanagedType.Bool)]
-    public static extern bool WinDivertRecvEx([In()] IntPtr handle, IntPtr pPacket, uint packetLen, ulong flags, ref WinDivertAddress pAddr, ref uint readLen, ref NativeOverlapped lpOverlapped);
+    public static extern bool WinDivertRecvEx([In] nint handle, nint pPacket, uint packetLen, ulong flags, ref WinDivertAddress pAddr, ref uint readLen, ref NativeOverlapped lpOverlapped);
 
-    /// Return Type: BOOL->int
-    ///handle: HANDLE->void*
-    ///pPacket: PVOID->void*
-    ///packetLen: UINT->unsigned int
-    ///pAddr: PWINDIVERT_ADDRESS->Anonymous_33ad92c9_0104_417e_989a_2fdd4b3efcc1*
-    ///writeLen: ref uint
+    /// <summary>
+    /// Send (write/inject) a packet to a WinDivert handle.
+    /// </summary>
+    /// <param name="handle">WinDivert handle.</param>
+    /// <param name="pPacket">Buffer containing the packet to send.</param>
+    /// <param name="packetLen">Packet length in bytes.</param>
+    /// <param name="pAddr">Packet metadata.</param>
+    /// <param name="writeLen">Receives number of bytes written.</param>
+    /// <returns><see langword="true"/> on success; otherwise <see langword="false"/>.</returns>
     [DllImport(WinDivert, EntryPoint = "WinDivertSend", CallingConvention = CallingConvention.Cdecl, SetLastError = true)]
     [return: MarshalAs(UnmanagedType.Bool)]
-    public static extern bool WinDivertSend([In()] IntPtr handle, [In()] IntPtr pPacket, uint packetLen, [In()] ref WinDivertAddress pAddr, ref uint writeLen);
+    public static extern bool WinDivertSend([In] nint handle, [In] nint pPacket, uint packetLen, [In] ref WinDivertAddress pAddr, ref uint writeLen);
 
-    /// Return Type: BOOL->int
-    ///handle: HANDLE->void*
-    ///pPacket: PVOID->void*
-    ///packetLen: UINT->unsigned int
-    ///flags: UINT64->unsigned __int64
-    ///pAddr: PWINDIVERT_ADDRESS->Anonymous_33ad92c9_0104_417e_989a_2fdd4b3efcc1*
-    ///writeLen: ref uint
-    ///lpOverlapped: LPOVERLAPPED->_OVERLAPPED*
+    /// <summary>
+    /// Send (write/inject) a packet to a WinDivert handle.
+    /// </summary>
+    /// <param name="handle">WinDivert handle.</param>
+    /// <param name="pPacket">Buffer containing the packet to send.</param>
+    /// <param name="packetLen">Packet length in bytes.</param>
+    /// <param name="flags">Send flags.</param>
+    /// <param name="pAddr">Packet metadata.</param>
+    /// <param name="writeLen">Receives number of bytes written.</param>
+    /// <param name="lpOverlapped">Overlapped I/O structure for asynchronous operations.</param>
+    /// <returns><see langword="true"/> on success; otherwise <see langword="false"/>.</returns>
     [DllImport(WinDivert, EntryPoint = "WinDivertSendEx", CallingConvention = CallingConvention.Cdecl, SetLastError = true)]
     [return: MarshalAs(UnmanagedType.Bool)]
-    public static extern bool WinDivertSendEx([In()] IntPtr handle, [In()] IntPtr pPacket, uint packetLen, ulong flags, [In()] ref WinDivertAddress pAddr, ref uint writeLen, ref NativeOverlapped lpOverlapped);
+    public static extern bool WinDivertSendEx([In] nint handle, [In] nint pPacket, uint packetLen, ulong flags, [In] ref WinDivertAddress pAddr, ref uint writeLen, ref NativeOverlapped lpOverlapped);
 
-    /// Return Type: BOOL->int
-    ///handle: HANDLE->void*
-    ///pPacket: PVOID->void*
-    ///packetLen: UINT->unsigned int
-    ///flags: UINT64->unsigned __int64
-    ///pAddr: PWINDIVERT_ADDRESS->Anonymous_33ad92c9_0104_417e_989a_2fdd4b3efcc1*
-    ///writeLen: ref uint
-    ///lpOverlapped: LPOVERLAPPED->_OVERLAPPED*
+    /// <summary>
+    /// Send (write/inject) a packet to a WinDivert handle.
+    /// </summary>
+    /// <param name="handle">WinDivert handle.</param>
+    /// <param name="pPacket">Buffer containing the packet to send.</param>
+    /// <param name="packetLen">Packet length in bytes.</param>
+    /// <param name="flags">Send flags.</param>
+    /// <param name="pAddr">Packet metadata.</param>
+    /// <param name="ignoredLenPtr">Ignored optional write length pointer.</param>
+    /// <param name="ignoredOverlappedPtr">Ignored optional overlapped pointer.</param>
+    /// <returns><see langword="true"/> on success; otherwise <see langword="false"/>.</returns>
     [DllImport(WinDivert, EntryPoint = "WinDivertSendEx", CallingConvention = CallingConvention.Cdecl, SetLastError = true)]
     [return: MarshalAs(UnmanagedType.Bool)]
-    public static extern bool WinDivertSendEx([In()] IntPtr handle, [In()] IntPtr pPacket, uint packetLen, ulong flags, [In()] ref WinDivertAddress pAddr, IntPtr ignoredLenPtr, IntPtr ignoredOverlappedPtr);
+    public static extern bool WinDivertSendEx([In] nint handle, [In] nint pPacket, uint packetLen, ulong flags, [In] ref WinDivertAddress pAddr, nint ignoredLenPtr, nint ignoredOverlappedPtr);
 
-    /// Return Type: BOOL->int
-    ///handle: HANDLE->void*
+    /// <summary>
+    /// Close a WinDivert handle.
+    /// </summary>
+    /// <param name="handle">WinDivert handle.</param>
+    /// <returns><see langword="true"/> on success; otherwise <see langword="false"/>.</returns>
     [DllImport(WinDivert, EntryPoint = "WinDivertClose", CallingConvention = CallingConvention.Cdecl, SetLastError = true)]
     [return: MarshalAs(UnmanagedType.Bool)]
-    public static extern bool WinDivertClose([In()] IntPtr handle);
+    public static extern bool WinDivertClose([In] nint handle);
 
-    /// Return Type: BOOL->int
-    ///handle: HANDLE->void*
-    ///param: WINDIVERT_PARAM->Anonymous_fff177c6_9a3b_4c59_b7f9_62aa58d87f4e
-    ///value: UINT64->unsigned __int64
+    /// <summary>
+    /// Set a WinDivert handle parameter.
+    /// </summary>
+    /// <param name="handle">WinDivert handle.</param>
+    /// <param name="param">Parameter to set.</param>
+    /// <param name="value">Parameter value.</param>
+    /// <returns><see langword="true"/> on success; otherwise <see langword="false"/>.</returns>
     [DllImport(WinDivert, EntryPoint = "WinDivertSetParam", CallingConvention = CallingConvention.Cdecl, SetLastError = true)]
     [return: MarshalAs(UnmanagedType.Bool)]
-    public static extern bool WinDivertSetParam([In()] IntPtr handle, WinDivertParam param, ulong value);
+    public static extern bool WinDivertSetParam([In] nint handle, WinDivertParam param, ulong value);
 
-    /// Return Type: BOOL->int
-    ///handle: HANDLE->void*
-    ///param: WINDIVERT_PARAM->Anonymous_fff177c6_9a3b_4c59_b7f9_62aa58d87f4e
-    ///pValue: UINT64*
+    /// <summary>
+    /// Get a WinDivert handle parameter.
+    /// </summary>
+    /// <param name="handle">WinDivert handle.</param>
+    /// <param name="param">Parameter to query.</param>
+    /// <param name="pValue">Receives parameter value.</param>
+    /// <returns><see langword="true"/> on success; otherwise <see langword="false"/>.</returns>
     [DllImport(WinDivert, EntryPoint = "WinDivertGetParam", CallingConvention = CallingConvention.Cdecl, SetLastError = true)]
     [return: MarshalAs(UnmanagedType.Bool)]
-    public static extern bool WinDivertGetParam([In()] IntPtr handle, WinDivertParam param, [Out()] out ulong pValue);
+    public static extern bool WinDivertGetParam([In] nint handle, WinDivertParam param, [Out] out ulong pValue);
 
-    /// Return Type: BOOL->int
-    ///pPacket: PVOID->void*
-    ///packetLen: UINT->unsigned int
-    ///ppIpHdr: PWINDIVERT_IPHDR*
-    ///ppIpv6Hdr: PWINDIVERT_IPV6HDR*
-    ///ppIcmpHdr: PWINDIVERT_ICMPHDR*
-    ///ppIcmpv6Hdr: PWINDIVERT_ICMPV6HDR*
-    ///ppTcpHdr: PWINDIVERT_TCPHDR*
-    ///ppUdpHdr: PWINDIVERT_UDPHDR*
-    ///ppData: PVOID*
-    ///pDataLen: ref uint
+    /// <summary>
+    /// Parse IPv4/IPv6/ICMP/ICMPv6/TCP/UDP headers from a raw packet.
+    /// </summary>
+    /// <param name="pPacket">Packet buffer.</param>
+    /// <param name="packetLen">Packet length in bytes.</param>
+    /// <param name="ppIpHdr">Receives IPv4 header pointer.</param>
+    /// <param name="ppIpv6Hdr">Receives IPv6 header pointer.</param>
+    /// <param name="ppIcmpHdr">Receives ICMP header pointer.</param>
+    /// <param name="ppIcmpv6Hdr">Receives ICMPv6 header pointer.</param>
+    /// <param name="ppTcpHdr">Receives TCP header pointer.</param>
+    /// <param name="ppUdpHdr">Receives UDP header pointer.</param>
+    /// <param name="ppData">Receives payload data pointer.</param>
+    /// <param name="pDataLen">Receives payload data length.</param>
+    /// <returns><see langword="true"/> on success; otherwise <see langword="false"/>.</returns>
     [DllImport(WinDivert, EntryPoint = "WinDivertHelperParsePacket", CallingConvention = CallingConvention.Cdecl, SetLastError = true)]
     [return: MarshalAs(UnmanagedType.Bool)]
-    public static extern bool WinDivertHelperParsePacket([In()] IntPtr pPacket, uint packetLen, IPv4Header** ppIpHdr, IPv6Header** ppIpv6Hdr, IcmpV4Header** ppIcmpHdr, IcmpV6Header** ppIcmpv6Hdr, TcpHeader** ppTcpHdr, UdpHeader** ppUdpHdr, byte** ppData, ref uint pDataLen);
+    public static extern bool WinDivertHelperParsePacket([In] nint pPacket, uint packetLen, IPv4Header** ppIpHdr, IPv6Header** ppIpv6Hdr, IcmpV4Header** ppIcmpHdr, IcmpV6Header** ppIcmpv6Hdr, TcpHeader** ppTcpHdr, UdpHeader** ppUdpHdr, byte** ppData, ref uint pDataLen);
 
-    /// Return Type: UINT->unsigned int
-    ///pPacket: PVOID->void*
-    ///packetLen: UINT->unsigned int
-    ///pAddr: PWINDIVERT_ADDRESS->Anonymous_33ad92c9_0104_417e_989a_2fdd4b3efcc1*
-    ///flags: UINT64->unsigned __int64
+    /// <summary>
+    /// Calculate IPv4/IPv6/ICMP/ICMPv6/TCP/UDP checksums.
+    /// </summary>
+    /// <param name="pPacket">Packet buffer.</param>
+    /// <param name="packetLen">Packet length in bytes.</param>
+    /// <param name="pAddr">Packet metadata.</param>
+    /// <param name="flags">Checksum calculation flags.</param>
+    /// <returns>The number of checksums recalculated.</returns>
     [DllImport(WinDivert, EntryPoint = "WinDivertHelperCalcChecksums", CallingConvention = CallingConvention.Cdecl, SetLastError = true)]
-    public static extern uint WinDivertHelperCalcChecksums(IntPtr pPacket, uint packetLen, [In()] ref WinDivertAddress pAddr, ulong flags);
+    public static extern uint WinDivertHelperCalcChecksums(nint pPacket, uint packetLen, [In] ref WinDivertAddress pAddr, ulong flags);
 
-    /// Return Type: UINT->unsigned int
-    ///pPacket: PVOID->void*
-    ///packetLen: UINT->unsigned int
-    ///pAddr: PWINDIVERT_ADDRESS->Anonymous_33ad92c9_0104_417e_989a_2fdd4b3efcc1*
-    ///flags: UINT64->unsigned __int64
+    /// <summary>
+    /// Calculate IPv4/IPv6/ICMP/ICMPv6/TCP/UDP checksums.
+    /// </summary>
+    /// <param name="pPacket">Packet buffer.</param>
+    /// <param name="packetLen">Packet length in bytes.</param>
+    /// <param name="ignoredAddress">Ignored optional packet metadata pointer.</param>
+    /// <param name="flags">Checksum calculation flags.</param>
+    /// <returns>The number of checksums recalculated.</returns>
     [DllImport(WinDivert, EntryPoint = "WinDivertHelperCalcChecksums", CallingConvention = CallingConvention.Cdecl, SetLastError = true)]
-    public static extern uint WinDivertHelperCalcChecksums(IntPtr pPacket, uint packetLen, [In()] IntPtr ignoredAddress, ulong flags);
+    public static extern uint WinDivertHelperCalcChecksums(nint pPacket, uint packetLen, [In] nint ignoredAddress, ulong flags);
 
-    /// Return Type: UINT->unsigned int
-    ///pPacket: PVOID->void*
-    ///packetLen: UINT->unsigned int
-    ///pAddr: PWINDIVERT_ADDRESS->Anonymous_33ad92c9_0104_417e_989a_2fdd4b3efcc1*
-    ///flags: UINT64->unsigned __int64
+    /// <summary>
+    /// Calculate IPv4/IPv6/ICMP/ICMPv6/TCP/UDP checksums.
+    /// </summary>
+    /// <param name="pPacket">Packet buffer.</param>
+    /// <param name="packetLen">Packet length in bytes.</param>
+    /// <param name="ignoredAddress">Ignored optional packet metadata pointer.</param>
+    /// <param name="flags">Checksum calculation flags.</param>
+    /// <returns>The number of checksums recalculated.</returns>
     [DllImport(WinDivert, EntryPoint = "WinDivertHelperCalcChecksums", CallingConvention = CallingConvention.Cdecl, SetLastError = true)]
-    public static extern uint WinDivertHelperCalcChecksums(byte* pPacket, uint packetLen, [In()] IntPtr ignoredAddress, ulong flags);
+    public static extern uint WinDivertHelperCalcChecksums(byte* pPacket, uint packetLen, [In] nint ignoredAddress, ulong flags);
 
-    /// Return Type: BOOL->int
-    ///filter: char*
-    ///layer: WINDIVERT_LAYER->Anonymous_13846946_b76a_4250_9642_c2122691f126
-    ///errorStr: char**
-    ///errorPos: ref uint
+    /// <summary>
+    /// Check the given filter string.
+    /// </summary>
+    /// <param name="filter">Filter string.</param>
+    /// <param name="layer">Packet processing layer.</param>
+    /// <param name="errorStr">Receives pointer to an error message on failure.</param>
+    /// <param name="errorPos">Receives error position in the filter string.</param>
+    /// <returns><see langword="true"/> if the filter is valid; otherwise <see langword="false"/>.</returns>
     [DllImport(WinDivert, EntryPoint = "WinDivertHelperCheckFilter", CallingConvention = CallingConvention.Cdecl, SetLastError = true)]
     [return: MarshalAs(UnmanagedType.Bool)]
-    public static extern bool WinDivertHelperCheckFilter([In()][MarshalAs(UnmanagedType.LPStr)] string filter, WinDivertLayer layer, char** errorStr, ref uint errorPos);
+    public static extern bool WinDivertHelperCheckFilter([In][MarshalAs(UnmanagedType.LPStr)] string filter, WinDivertLayer layer, char** errorStr, ref uint errorPos);
 
-    /// Return Type: BOOL->int
-    ///filter: char*
-    ///layer: WINDIVERT_LAYER->Anonymous_13846946_b76a_4250_9642_c2122691f126
-    ///pPacket: PVOID->void*
-    ///packetLen: UINT->unsigned int
-    ///pAddr: PWINDIVERT_ADDRESS->Anonymous_33ad92c9_0104_417e_989a_2fdd4b3efcc1*
+    /// <summary>
+    /// Evaluate the given filter string.
+    /// </summary>
+    /// <param name="filter">Filter string.</param>
+    /// <param name="layer">Packet processing layer.</param>
+    /// <param name="pPacket">Packet buffer to evaluate.</param>
+    /// <param name="packetLen">Packet length in bytes.</param>
+    /// <param name="pAddr">Packet metadata.</param>
+    /// <returns><see langword="true"/> if the packet matches the filter; otherwise <see langword="false"/>.</returns>
     [DllImport(WinDivert, EntryPoint = "WinDivertHelperEvalFilter", CallingConvention = CallingConvention.Cdecl, SetLastError = true)]
     [return: MarshalAs(UnmanagedType.Bool)]
-    public static extern bool WinDivertHelperEvalFilter([In()][MarshalAs(UnmanagedType.LPStr)] string filter, WinDivertLayer layer, [In()] IntPtr pPacket, uint packetLen, [In()] ref WinDivertAddress pAddr);
+    public static extern bool WinDivertHelperEvalFilter([In][MarshalAs(UnmanagedType.LPStr)] string filter, WinDivertLayer layer, [In] nint pPacket, uint packetLen, [In] ref WinDivertAddress pAddr);
 }
